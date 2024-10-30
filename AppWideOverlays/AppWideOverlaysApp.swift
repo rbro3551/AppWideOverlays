a9e5dd376d4560785a1322d775900fa6aac6ce5f
Riley B <30447344+rbro3551@users.noreply.github.com>
2024-10-29 20:59:18 -0500

    Initial Commit

---
+++ AppWideOverlays/AppWideOverlaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppWideOverlaysApp.swift
+//  AppWideOverlays
+//
+//  Created by Riley Brookins on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct AppWideOverlaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
